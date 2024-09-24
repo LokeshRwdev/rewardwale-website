8962d16b7a7ce04d7d43488c4e18a1b88cddf33e
lokeshnandan <lokeshnandan007@gmail.com>
2024-09-24 16:42:30 +0530

    adding basic packages

---
+++ src/styles/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    darkMode: 'class', // Enable dark mode with a class
+    theme: {
+      extend: {
+        // You can extend the theme here if needed
+      },
+    },
+    plugins: [],
+  };
+  
